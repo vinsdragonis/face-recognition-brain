557d1dc04a4350f2a88a85f80ec98f7b3871ba30
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2021-09-16 03:49:13 +0530

    Update FaceRecgonition.js

--- src/components/FaceRecgonition/FaceRecgonition.js
+++ src/components/FaceRecgonition/FaceRecgonition.js
@@ -5,7 +5,7 @@ const FaceRecgonition = ({ imageUrl, box }) => {
   return (
     <div className="center ma">
       <div className="absolute mt-2 pa5">
-        <img id="inputImage" alt="" src={ imageUrl } width="500px" height="auto" />
+        <img id="inputImage" alt="" src={ imageUrl } width="300px" height="auto" />
         <div className="bounding-box" style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
       </div>
     </div>
